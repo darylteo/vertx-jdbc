ffb307784cd808d5522399ee7fe88418ba1e64cf
Daryl Teo <i.am@darylteo.com>
2012-06-26 22:11:44 +1000

    BusMod now pulls driver/url details from json file Author: Daryl Teo
    Details about which driver should be used, as well as
     the database to connect to (with username, password
     details) is now pulled from the config.json file.
    
    JDBC.java is converted to use Java 7 try-with-resource
    constructs rather than traditional try-finally structures.

--- jdbc.java
+++ jdbc.java
@@ -1,36 +1,52 @@
 import org.vertx.java.core.*;
+import org.vertx.java.core.json.*;
 import org.vertx.java.deploy.*;
 
 import java.sql.*;
 
 public class jdbc extends Verticle {
-   
-   public void start(){
-      
-      final String connectionString = "jdbc:postgresql:jdbctest";
-      final String username = "jdbctest";
-      final String password = "jdbctest";
+  
+   private class Config {
+      public String driver    = "";
+      public String url       = "";
 
-      Connection conn = null;
+      public String username  = "";
+      public String password  = "";
+   }
+
+   public void start(){
+     
+      final Config configuration = loadConfig(container.getConfig());
 
       try{
-         /*
-          * Supposedly with JDBC 4 this should not be required,
-          * but I can't seem to get it to work without it...
-          * Someone tell me what I'm missing ~dteo 2012-06-26
-          */
-         Class.forName("org.postgresql.Driver"); 
-
-         conn = DriverManager.getConnection(
-            connectionString,
-            username,
-            password
+         loadDriver(configuration.driver);
+      }catch(ClassNotFoundException e){
+         container.getLogger().fatal(
+            String.format(
+               "Could not load JDBC driver %s",
+               configuration.driver
+            ),
+            e
          );
 
+         return;
+      }
+
+      try(
+         Connection conn = DriverManager.getConnection(
+            configuration.url,
+            configuration.username,
+            configuration.password
+         );
+      ){
+         container.getLogger().info(
+            String.format(
+               "Connected to Database \"%s\"",
+               configuration.url
+            )
+         );
       }catch(Exception e){
          e.printStackTrace();
-      }finally{
-         closeConnection(conn);
       }
 
    }
@@ -45,7 +61,33 @@ public class jdbc extends Verticle {
       }catch(SQLException e){
          e.printStackTrace();
       }
-      
    }
 
+   private Config loadConfig(JsonObject config){
+      Config result = new Config();
+
+      if(config == null){
+         return result;
+      }
+
+      result.driver = config.getString("driver");
+      result.url = config.getString("url");
+      result.username = config.getString("username");
+      result.password = config.getString("password");
+      
+      return result;
+   }
+
+   private void loadDriver(String driver) throws ClassNotFoundException{
+      /*
+       * Supposedly with JDBC 4 this should not be required,
+       * but I can't seem to get it to work without it...
+       * Someone tell me what I'm missing ~dteo 2012-06-26
+       */
+      if(driver.isEmpty()){
+         return;
+      }
+      
+      Class.forName(driver); 
+   }
 }
