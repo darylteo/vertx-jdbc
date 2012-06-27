60dec8ef1082858b36d5d82e40fa55e06c40f349
Daryl Teo <i.am@darylteo.com>
2012-06-27 16:53:47 +1000

    SelectHandler handles subset of types for params
    SelectHandler can now map a subset of SQL types
    to prepared statements.

--- jdbc/handlers/SelectHandler.java
+++ jdbc/handlers/SelectHandler.java
@@ -19,13 +19,20 @@ public class SelectHandler extends JdbcHandler {
       System.out.println("Message Received: " + message.body);
 
       try(
-         Connection conn = super.openConnection()
+         final Connection conn = super.openConnection()
       ){
          final String query = message.body.getString("query"); 
+         final JsonArray params = message.body.getArray("params");
 
          final PreparedStatement stmt = conn.prepareStatement(
             query
          );
+
+         int count = 0;
+         for(Object param : params){
+            count ++;
+            setParameter(stmt,(JsonObject)param,count);
+         }
 
          final ResultSet rs = stmt.executeQuery();
          final JsonObject result = resultSetToJsonObject(rs);
@@ -33,6 +40,45 @@ public class SelectHandler extends JdbcHandler {
          message.reply(result);
       }catch(SQLException e){
          e.printStackTrace();
+      }
+   }
+
+
+   private void setParameter(
+      PreparedStatement stmt,
+      JsonObject param,
+      int index
+   ) throws SQLException {
+      final String type = param.getString("type");
+
+
+      /* Information about mapping SQL Types to Java Types can 
+       * be found here
+       * http://docs.oracle.com/javase/1.5.0/docs/guide/jdbc/
+            getstart/mapping.html 
+       **/
+      switch(type){
+         /* TINYINT and SMALLINT map to Short */
+         case "TINYINT":
+         case "SMALLINT":
+            stmt.setShort(index,param.getNumber("value").shortValue());
+            break;
+
+         case "INTEGER":
+            stmt.setInt(index,param.getNumber("value").intValue());
+            break;
+
+         case "BIGINT":
+            stmt.setLong(index,param.getNumber("value").longValue());
+            break;
+
+         /* String Types*/
+         case "CHAR":
+         case "NCHAR":
+         case "VARCHAR":
+         case "NVARCHAR":
+            stmt.setString(index,param.getString("value"));
+            break;
       }
    }
 }
